a1e2884ad1e1b844cb8bb96866a75a285631c3e6
Tanzka <102594523+Tanzka@users.noreply.github.com>
2025-10-30 14:57:31 +0200

    osa 2

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,6 +1,18 @@
 import { useState, useEffect } from 'react'
 import personService from './services/persons'
 
+const Notification = ({message, className}) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className={className}>
+      {message}
+    </div>
+  )
+}
+
 const Filter = ({filter, onChange}) => (
   <div>
     Filter shown with:
@@ -24,11 +36,14 @@ const PersonsForm = ({addPerson, newName, handleNameChange, newNumber, handleNum
   </form>
 )
 
-const Persons = ({persons}) => (
+const Persons = ({persons, removePerson}) => (
   <ul>
     {persons.map(person =>
       <li key={person.name}>
-        {person.name} {person.number}
+        {person.name} {person.number}{' '}
+        <button onClick={() => removePerson(person.name, person.id)}>
+          Delete
+        </button>
       </li>
     )}
   </ul>
@@ -39,6 +54,8 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     personService
@@ -48,25 +65,72 @@ const App = () => {
       })
   }, [])
 
+  const notification = (message) => {
+    setMessage(message)
+    setTimeout(() => setMessage(null), 5000)
+  }
+
+  const errorMessage = (message) => {
+    setError(message)
+    setTimeout(() => setError(null), 5000)
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
 
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to the phonebook`)
+    const existingPerson = persons.find(person => person.name === newName)
+
+    if(existingPerson) {
+      const confirmUpdate = window.confirm(
+        `${newName} is already added to phonebook, replace the old number with new one?`
+      )
+
+      if (confirmUpdate) {
+        const updatedPerson = { ...existingPerson, number: newNumber }
+
+        personService 
+          .update(existingPerson.id, updatedPerson)
+          .then(returnedPerson => {
+            setPersons(persons.map(p => p.id !== existingPerson.id ? p : returnedPerson))
+            setNewName('')
+            setNewNumber('')
+            notification(`Updated number for ${returnedPerson.name}`)
+          })
+          .catch(error => {
+            errorMessage(
+              `${existingPerson.name} has already been removed from the server`
+            )
+            setPersons(persons.filter(p => p.id !== existingPerson.id))
+          })
+      }
       return
     }
 
-    const personObject = { 
-      name: newName,
-      number: newNumber 
-    }
-    
-    personService 
-      .create(personObject)
+    const newPerson = { name: newName, number: newNumber }
+
+    personService
+      .create(newPerson)
       .then(returnedPerson => {
         setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
+        notification(`Added ${returnedPerson.name}`)
+      })
+  }
+
+  const removePerson = (name, id) => {
+    const confirmDelete = window.confirm(`Delete ${name}?`)
+    if(!confirmDelete) return
+
+    personService 
+      .remove(id)
+      .then(() => {
+        setPersons(persons.filter(person => person.id !== id))
+        notification(`Deleted ${name}`)
+      })
+      .catch(() => {
+        errorMessage(`${name} was already removed from the server.`)
+        setPersons(persons.filter(person => person.id !== id))
       })
   }
 
@@ -74,9 +138,15 @@ const App = () => {
     person.name.toLowerCase().includes(filter.toLowerCase())
   )
 
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <Notification message={message} className={"notification"} />
+      <Notification message={error} className={"error"} />
+      
       <Filter filter={filter} onChange={(event) => setFilter(event.target.value)} />
 
       <h3>Add a new</h3>
@@ -89,7 +159,7 @@ const App = () => {
       />
       
       <h3>Numbers</h3>
-      <Persons persons={personsToShow} />
+      <Persons persons={personsToShow} removePerson={removePerson} />
     </div>
   )
 
