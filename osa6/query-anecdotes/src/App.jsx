02d8105ef63eaad9519ae104225c91ccaf15ea0b
Tanzka <102594523+Tanzka@users.noreply.github.com>
2026-01-14 16:57:39 +0200

    Query

---
+++ osa6/query-anecdotes/src/App.jsx
@@ -0,0 +1,67 @@
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
+import { useNotificationDispatch } from './NotificationContext'
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+  const queryClient = useQueryClient()
+  const dispatch = useNotificationDispatch()
+
+  const voteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAnecdote) => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+
+      dispatch({ type: 'SET', payload: `voted for '${updatedAnecdote.content}'` })
+
+      setTimeout(() => {
+        dispatch({ type: 'CLEAR' })
+      }, 5000)
+    }
+  })
+
+  const handleVote = (anecdote) => {
+    voteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
+    console.log('voting for:', anecdote.content)
+  }
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false 
+  })
+
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
---
+++ osa6/query-anecdotes/src/requests.js
@@ -0,0 +1,40 @@
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAnecdotes = async () => {
+    const response = await fetch(baseUrl)
+    if (!response.ok) {
+        throw new Error('Failed to fetch anecdotes')
+    }
+    return await response.json()
+}
+
+export const createAnecdote = async (newAnecdote) => {
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newAnecdote)
+    }
+
+    const response = await fetch(baseUrl, options)
+
+    if (!response.ok) {
+        const errorData = await response.json()
+        throw new Error(errorData.error)
+    }
+
+    return await response.json()
+}
+
+export const updateAnecdote = async (updatedAnecdote) => {
+    const response = await fetch(`${baseUrl}/${updatedAnecdote.id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(updatedAnecdote)
+    })
+
+    if (!response.ok) {
+        throw new Error('Failed to create anecdote')
+    }
+
+    return await response.json()
+}
